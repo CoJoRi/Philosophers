abd5ca7c8c38cf9b9e78c2bffebb26be144b9d71
CoJoRi <rinaudo.joseph12@gmail.com>
2025-03-13 09:13:06 +0100

    philo bonus corrected

--- philobonusCorrected/philo2.c
+++ philobonusCorrected/philo2.c
@@ -6,7 +6,7 @@
 /*   By: jrinaudo <jrinaudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 17:12:49 by jrinaudo          #+#    #+#             */
-/*   Updated: 2025/02/08 17:17:34 by jrinaudo         ###   ########.fr       */
+/*   Updated: 2025/03/06 13:28:15 by jrinaudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ int	is_alive(t_philo *philo)
 	if (duration > philo->table->time_die)
 	{
 		sem_wait(philo->table->dead);
-		message(philo, RED"died"RESET);
+		sem_wait(philo->table->message);
+		printf(""BG_WHITE GREY"%ld "BLUE"%d "RED" died\n"RESET,
+		get_time_elapsed(philo->table->time_start), philo->id);
 		release_fork(philo);
 		return (0);
 	}
